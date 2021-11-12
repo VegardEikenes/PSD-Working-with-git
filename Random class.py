e8095a7a8d871c13f6e9b16856eb9913d9143c98
Vegard Eikenes <vegardteikenes@gmail.com>
2021-11-12 16:44:51 +0100

    Final commit, added constructor to class

--- Random class.py
+++ Random class.py
@@ -3,10 +3,15 @@ from random import randint
 
 class Random():
 
-    def randomnumber(self, intamount, intmin, intmax):
-        for n in range(intamount):
-            print(randint(intmin, intmax))
+    def __init__(self, intamount, intmin, intmax):
+        self.intamount = intamount
+        self.intmin = intmin
+        self.intmax = intmax
 
+    def randomnumber(self):
+        for n in range(self.intamount):
+            print(randint(self.intmin, self.intmax))
+            
 if __name__ == "__main__":
-    test = Random()
-    test.randomnumber(20, 1, 100)
+    test = Random(10, 1, 20)
+    test.randomnumber()
